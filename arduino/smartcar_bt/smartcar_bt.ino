45ddacac59b8e0439e3c2d23e896dd28a21277df
Mary Olsson Radda <56348164+marols97@users.noreply.github.com>
2020-05-11 21:24:39 +0200

    Update smartcar_bt.ino

--- arduino/smartcar_bt/smartcar_bt.ino
+++ arduino/smartcar_bt/smartcar_bt.ino
@@ -19,8 +19,8 @@ bool automaticDriving;
 const unsigned long PRINT_INTERVAL = 100;
 unsigned long previousPrintout = 0;
 const auto pulsesPerMeter = 600;
-const char* ssid     =  "AndroidAP";
-const char* password = "arwy0176";
+const char* ssid     =  "ssid";
+const char* password = "password";
 WiFiServer server(80);
 
 BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
