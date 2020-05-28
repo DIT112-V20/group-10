0c02dc4c3648259ecef5be85839b51e6753a9810
Effat Mahmud Enti <62544319+efenaty@users.noreply.github.com>
2020-05-28 19:02:52 +0200

    Update MapsActivity.java

--- android/WiFi/app/src/main/java/com/example/wifi/MapsActivity.java
+++ android/WiFi/app/src/main/java/com/example/wifi/MapsActivity.java
@@ -34,12 +34,6 @@ import static java.lang.System.in;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final String LOG_TAG = "MapActivity";
-    //private static final String url = RequestHelper.magess.getUrl().toString() + "/M";
-    //http://www.mocky.io/v2/5ece50ae3000005000ea1045
-
-    //private static final CharSequence[] MAP_TYPE_ITEMS = {"Road Map", "Hybrid", "Satellite", "Terrain"};
-
-    //private static final String SERVICE_URL = RequestHelper.magess.getUrl().toString() + "/M";
     private GoogleMap map;
 
 
@@ -91,45 +85,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     protected void retrieveAndAddCities() throws IOException {
-        //System.out.println(SERVICE_URL);
         String response = RequestHelper.mapCoordinatesRequestToServer();
-
-        System.out.println("The body is in mapactivity is " + response);
-//        final StringBuilder json = new StringBuilder();
-//        try {
-//            // Connect to the web service
-//            //URL url = new URL(SERVICE_URL);
-//            //System.out.println(SERVICE_URL);
-//            //conn = (HttpURLConnection) url.openConnection();
-//            //InputStreamReader in = new InputStreamReader(conn.getInputStream());
-//
-//            // Read the data into the StringBuilder
-////            int read;
-////            char[] buff = new char[1024];
-////            while (response != "<br>") {
-////                json.append(response);
-////                //System.out.println("My values are changing "+ json);
-////            }
-//
-//         //   System.out.print("My json String is "+ json.toString());
-//
-//            //System.out.print("My json before was " + json.substring(0,4) + " ");
-//            //System.out.println("My json before was " + json.length() + " ");
-//            //System.out.println();
-//        } catch (IOException e) {
-//            Log.e(LOG_TAG, "Error connecting to service", e);
-//            throw new IOException("Error connecting to service", e);
-//        } finally {
-//            if (conn != null) {
-//                conn.disconnect();
-//            }
 
          //Create markers for the city data.
         // Must run this on the UI thread since it's a UI operation.
         runOnUiThread(new Runnable() {
             public void run() {
                 try {
-                    //System.out.println("My values are "+ json.toString());
+    
                     createMarkersFromJson(response);
                 } catch (JSONException e) {
                     Log.e(LOG_TAG, "Error processing JSON", e);
@@ -146,44 +109,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         JSONObject jsonObj = new JSONObject();
 
         String []array = json.split(" ");
-        System.out.println("My array length " + array.length);
-        System.out.println("My first element "+ array[0]);
-        System.out.println("My second element "+ array[1]);
+        
         String []lat = array[1].split("\n");
-        System.out.println("My lat length is " + lat.length);
-        System.out.println("My third element "+ array[2]);
-        //String []lat = array[0].split(":");
-        //String lon[] = array[1].split(":");
-        //String latitude = lat[0].trim();
-//        String latVal = lat[1].trim();
-//        String longitude = lon[0].trim();
-//        String lonVal = lon[1].trim();
-//        String lati[] = new String[lat.length];
-//        String longi[] = new String[lon.length];
-//        lati[0] = latitude;
-//        lati[1] = latVal;
-//        longi[0] = longitude;
-//        longi[1] = lonVal;
-
-        //System.out.println("My first lat is " + array[0]);
-        //System.out.println("My lat is "+ array[1]);
-        //System.out.println("My lon is "+ array[3]);
-        //String []lat = json.split("<br>");
-//        System.out.println("My array size is "+ lat.length);
-//        System.out.println("My first value "+ lat[0]);
-//        System.out.println("My second value "+ lat[1]);
-        //System.out.println("My third value "+ array[2]);
-        jsonObj.put(array[0], 57.706478);
-                //lat[0]);
-        jsonObj.put(lat[1], 11.9374733);
-                //array[2]);
+        
+    
+        jsonObj.put(array[0],lat[0]);
+        jsonObj.put(lat[1], array[2]);
         System.out.println("My Objects are "+ jsonObj);
         //LatLng magess = new LatLng(57.706478,11.9374733);
         LatLng magess = new LatLng(jsonObj.getDouble("Lat:"),jsonObj.getDouble("Lon:"));
-        //Print statements were mainly used for figuring out the JsonObject
-        //But gonna leave it here as comments for later activities.
-        //System.out.println("My whaaat 1 "+ jsonObj.getDouble("lat"));
-        //System.out.println("My whaaat 2 "+ jsonObj.getDouble("lng"));
+        
         map.addMarker(new MarkerOptions().position(magess).title("Marker of Magess"));
         map.moveCamera(CameraUpdateFactory.newLatLng(magess));
 
